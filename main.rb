f9e49d44be1ca36397b1c70dfa9fe7583c66813c
Paula Rocha <100885530+Paularina@users.noreply.github.com>
2023-02-17 09:41:36 -0300

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade:"
+idade = gets.chomp.to_i
+
+puts "Olá #{nome} #{sobrenome} que tem #{idade} anos!"
